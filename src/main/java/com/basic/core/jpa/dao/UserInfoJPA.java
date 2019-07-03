684ddf9574580fd39a3423bdb4522a0e194eefef
kunge2013 <543976974@QQ.com>
2019-07-04 02:58:17 +0800

    update

--- src/main/java/com/basic/core/jpa/dao/UserInfoJPA.java
+++ src/main/java/com/basic/core/jpa/dao/UserInfoJPA.java
@@ -2,6 +2,7 @@ package com.basic.core.jpa.dao;
 
 import com.basic.core.jpa.bean.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -13,6 +14,6 @@ import java.util.List;
 public interface UserInfoJPA
         extends JpaRepository<UserInfo, Long>
 {
+    @Query("select t from UserInfo t where t.userName =?1 and t.passWord=?2")
     List<UserInfo> findUserInfo(String userName, String passWord);
-
 }
